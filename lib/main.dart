37b5066922ecbcaa37a616a511996984a5d2db9d
adoredson <mainajohn2580@mail.com>
2025-08-05 15:25:36 +0300

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/community.dart';
+import 'pages/testimony.dart';
+import 'pages/devotion.dart';
+import 'pages/my_Profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +16,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'The Cenacle',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('The Cenacle'),
-          centerTitle: true,
-          leading: Icon(Icons.home),
-        ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    CommunityPage(),
+    TestimonyPage(),
+    DevotionPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<String> _titles = [
+    'Home',
+    'Community',
+    'Testimony',
+    'Devotion',
+    'Profile',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_titles[_selectedIndex]), centerTitle: true),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.tealAccent,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Community'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.record_voice_over),
+            label: 'Testimony',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Devotion'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
     );
   }
---
+++ lib/pages/devotion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DevotionPage extends StatelessWidget {
+  const DevotionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Welcome to Devotion Page'));
+  }
+}
